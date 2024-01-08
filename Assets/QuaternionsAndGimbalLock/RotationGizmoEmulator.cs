2c7f0c38b2d992c1323c75e3dd2ae5aa3958fb12
Wojtek Sterna <wojtsterna@gmail.com>
2024-01-08 17:14:02 +0100

    .

---
+++ Assets/QuaternionsAndGimbalLock/RotationGizmoEmulator.cs
@@ -0,0 +1,38 @@
+using UnityEngine;
+
+public class RotationGizmoEmulator : MonoBehaviour
+{
+	public enum CoordSystem { Global, Local }
+
+	public CoordSystem coordSystem;
+	public bool x = false;
+	public bool y = false;
+	public bool z = false;
+	public float speed = 30.0f;
+
+	void Update()
+    {
+		Quaternion currentRotation = transform.rotation;
+
+		if (coordSystem == CoordSystem.Global)
+		{
+			if (x)
+				currentRotation = Quaternion.AngleAxis(Time.deltaTime * speed, new Vector3(1.0f, 0.0f, 0.0f)) * currentRotation;
+			if (y)
+				currentRotation = Quaternion.AngleAxis(Time.deltaTime * speed, new Vector3(0.0f, 1.0f, 0.0f)) * currentRotation;
+			if (z)
+				currentRotation = Quaternion.AngleAxis(Time.deltaTime * speed, new Vector3(0.0f, 0.0f, 1.0f)) * currentRotation;
+		}
+		else
+		{
+			if (x)
+				currentRotation = Quaternion.AngleAxis(Time.deltaTime * speed, transform.right) * currentRotation;
+			if (y)
+				currentRotation = Quaternion.AngleAxis(Time.deltaTime * speed, transform.up) * currentRotation;
+			if (z)
+				currentRotation = Quaternion.AngleAxis(Time.deltaTime * speed, transform.forward) * currentRotation;
+		}
+
+		transform.rotation = currentRotation;
+	}
+}
---
+++ Assets/QuaternionsAndGimbalLock/RotationOverride.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+public class RotationOverride : MonoBehaviour
+{
+	public Vector3 euler;
+
+    void Update()
+    {
+		transform.rotation = Quaternion.Euler(0.0f, euler.y, 0.0f) * Quaternion.Euler(euler.x, 0.0f, 0.0f) * Quaternion.Euler(0.0f, 0.0f, euler.z);
+	//	transform.rotation = Quaternion.Euler(euler);
+	}
+}
